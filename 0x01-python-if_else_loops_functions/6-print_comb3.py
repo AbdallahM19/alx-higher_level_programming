ef62dda8cbb24724b303c6aeab22bef5560d24f2
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-11-28 16:22:36 -0800

    Python

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for num in range(10):
+    for i in range(num + 1, 10):
+        if num != i:
+            if num == 8 and num == 9:
+                print("{:d}{:d}".format(num, i))
+            else:
+                print("{:d}{:d}, ".format(num, i), end="")
+
