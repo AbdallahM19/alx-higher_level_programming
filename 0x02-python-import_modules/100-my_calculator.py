585508c0510037e7be53464168d2f906be45929d
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-11-30 06:34:54 -0800

    Python

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -13,15 +13,14 @@ def calculator(a, operator, b):
         result = div(int(a), int(b))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
+        exit(1)
 
     print("{} {} {} = {}".format(a, operator, b, result))
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+        exit(1)
 
     a, operator, b = sys.argv[1], sys.argv[2], sys.argv[3]
     calculator(a, operator, b)
-
