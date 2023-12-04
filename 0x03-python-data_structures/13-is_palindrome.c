6ddb9a152d0b10e51ff14883cade7bcb2a22530f
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-12-03 16:50:30 -0800

    python

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,32 +1,73 @@
 #include "lists.h"
 
 /**
+ * rev_list - reverses linked list
+ * @head: pointer head of the linked list
+ * Return: new head of the reversed list
+ */
+void rev_list(listint_t **head)
+{
+	listint_t *prev = NULL;
+	listint_t *current = *head;
+	listint_t *next = NULL;
+
+	while (current)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+	*head = prev;
+}
+
+/**
+ * compare_lists - compares two linked lists for equality
+ * @list1: pointer first linked list
+ * @list2: pointer second linked list
+ * Return: 1 if lists are equal, 0 if (not equal)
+ */
+listint_t *compare_lists(listint_t *head)
+{
+	listint_t *slow = head;
+	listint_t *fast = head;
+
+	while (fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	return (slow);
+}
+
+/**
  * is_palindrome - checks if a singly linked list is a palindrome
- * @head: pointer to head of list
- * Return: 0 if not a palindrome, 1 if it is a palindrome
+ * @head: double pointer to the head of the linked list
+ * Return: 1 if palindrome, 0 otherwise
  */
 int is_palindrome(listint_t **head)
 {
-    listint_t *current = *head;
-    int count = 0, i, j, arr[100000];
+	listint_t *slow, *fast, *second_half;
 
-    if (*head == NULL || (*head)->next == NULL)
-        return (1);
-    while (current != NULL)
-    {
-        current = current->next;
-        count++;
-    }
-    current = *head;
-    for (i = 0; i < count; i++)
-    {
-        arr[i] = current->n;
-        current = current->next;
-    }
-    for (i = 0, j = count - 1; i < count / 2; i++, j--)
-    {
-        if (arr[i] != arr[j])
-            return (0);
-    }
-    return (1);
+	if (*head == NULL || (*head)->next == NULL)
+		return(1);
+
+	slow = compare_lists(*head);
+	fast = slow->next;
+	rev_list(&fast);
+	second_half = *head;
+
+	while (fast && second_half)
+	{
+		if (fast->n == second_half->n)
+		{
+			fast = fast->next;
+			second_half = second_half->next;
+		}
+		else
+			return(0);
+	}
+	if (!fast)
+		return (1);
+	return (0);
 }
