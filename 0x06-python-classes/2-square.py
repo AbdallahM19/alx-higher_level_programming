f4855422ee08108cb2ebfa258dfa01ed192562e1
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-12-20 00:32:13 +0200

    class

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-"""Define my square"""
+"""
+Define
+my square
+"""
 
 
 class Square:
     """My Private instance attribute"""
 
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
