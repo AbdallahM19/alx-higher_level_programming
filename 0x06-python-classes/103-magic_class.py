759e3dc8733517a9e60e534387d995000e06cdf6
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-12-20 01:55:02 +0200

    class

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,41 +1,30 @@
-#!/usr/bin/python3 -u
+#!/usr/bin/python3
+"""
+Define
+my square
+"""
+import math
 
-import ctypes
 
-lib = ctypes.CDLL('./libPython.so')
-lib.print_python_list.argtypes = [ctypes.py_object]
-lib.print_python_bytes.argtypes = [ctypes.py_object]
-lib.print_python_float.argtypes = [ctypes.py_object]
-s = b"Hello"
-lib.print_python_bytes(s);
-b = b'\xff\xf8\x00\x00\x00\x00\x00\x00';
-lib.print_python_bytes(b);
-b = b'What does the \'b\' character do in front of a string literal?';
-lib.print_python_bytes(b);
-l = [b'Hello', b'World']
-lib.print_python_list(l)
-del l[1]
-lib.print_python_list(l)
-l = l + [4, 5, 6.0, (9, 8), [9, 8, 1024], b"School", "Betty"]
-lib.print_python_list(l)
-l = []
-lib.print_python_list(l)
-l.append(0)
-lib.print_python_list(l)
-l.append(1)
-l.append(2)
-l.append(3)
-l.append(4)
-lib.print_python_list(l)
-l.pop()
-lib.print_python_list(l)
-l = ["School"]
-lib.print_python_list(l)
-lib.print_python_bytes(l);
-f = 3.14
-lib.print_python_float(f);
-l = [-1.0, -0.1, 0.0, 1.0, 3.14, 3.14159, 3.14159265, 3.141592653589793238462643383279502884197169399375105820974944592307816406286]
-print(l)
-lib.print_python_list(l);
-lib.print_python_float(l);
-lib.print_python_list(f);
+class MagicClass:
+    """My Private instance attribute"""
+
+    def __init__(self, radius=0):
+        self.__radius = 0
+        self.radius = radius
+
+    @property
+    def radius(self):
+        return self.__radius
+
+    @radius.setter
+    def radius(self, value):
+        if type(value) is not int and type(value) is not float:
+            raise TypeError("radius must be a number")
+        self.__radius = value
+
+    def area(self):
+        return math.pi * self.__radius**2
+
+    def circumference(self):
+        return math.pi * self.__radius * 2
