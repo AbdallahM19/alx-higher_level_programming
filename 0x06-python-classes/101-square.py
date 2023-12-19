503bf9f7c06170e3bd1789c9a50c8c3a8ddb96a1
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-12-20 01:30:44 +0200

    class

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,10 +1,61 @@
 #!/usr/bin/python3
-import sys
+"""
+Define
+my Square
+"""
 
-def safe_function(fct, *args):
-    try:
-        result = fct(*args)
-        return result
-    except Exception as e:
-        print("Exception: {}".format(e), file=sys.stderr)
-        return None
+
+class Square:
+    """My Private instance attribute"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if (
+            not isinstance(value, tuple)
+            or len(value) != 2
+            or not all(isinstance(i, int) and i >= 0 for i in value)
+        ):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
+    def area(self):
+        return self.__size**self.__size
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__position[1]):
+                print()
+            for k in range(self.__size):
+                print(" " * self.__position[0] + "#" * self.__size)
+
+    def __str__(self):
+        result = []
+        for x in range(self.__position[1]):
+            result.append("")
+        for i in range(self.__size):
+            result.append(" " * self.__position[0] + "#" * self.__size)
+        return "\n".join(result)
