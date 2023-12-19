c5cd335f9ac6afc2855b11f6271ada295e52d6f0
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-12-20 01:50:28 +0200

    class

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,15 +1,45 @@
 #!/usr/bin/python3
-def magic_calculation(a, b):
-    result = 0
+"""
+Define
+my Square
+"""
 
-    for i in range(1, 3):
-        try:
-            if i > a:
-                raise Exception('Too far')
-            result += (a ** b) / i
 
-        except:
-            result = b + a
-            break
+class Square:
+    """My Private instance attribute"""
 
-    return result
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size**2
+
+    def __eq__(self, other):
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        return self.area() != other.area()
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        return self.area() <= other.area()
+
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        return self.area() >= other.area()
