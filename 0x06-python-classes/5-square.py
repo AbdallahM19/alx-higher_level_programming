dc0c4340b17f9b4a7fa30a38b17f0679339ad00b
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-12-20 00:39:24 +0200

    class

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -6,29 +6,14 @@ class Square:
     """My Private instance attribute"""
 
     def __init__(self, size = 0):
-        """
-        Instantiation
-        with optional
-        """
-
         self.__size = size
 
     @property
     def size(self):
-        """
-        property def size(self):
-        to retrieve it
-        """
-
         return self.__size
 
     @size.setter
     def size(self, value):
-        """
-        Private instance
-        attribute
-        """
-
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -36,13 +21,9 @@ class Square:
         self.__size = value
 
     def area(self):
-        """Public instance method"""
-
         return (self.__size ** 2)
     
     def my_print(self):
-        """print the square in #"""
-
         if self.__size == 0:
             print()
         else:
