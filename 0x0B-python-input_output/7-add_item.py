bc3da6b0942fe47eb05996461e63975478c03a7f
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-01-10 01:51:00 +0200

    input

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,8 +5,8 @@ a Python list and save them to a file
 """
 
 import sys
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 try:
     file_list = load_from_json_file("add_item.json")
