f56fa2dbc1fa5e606fac26a9a8fbadc1307b2a56
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-01-10 01:36:33 +0200

    input

--- 0x0B-python-input_output/11-main.py
+++ 0x0B-python-input_output/11-main.py
@@ -2,10 +2,10 @@
 import os
 import sys
 
-Student = __import__('11-student').Student
-read_file = __import__('0-read_file').read_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+Student = __import__("11-student").Student
+read_file = __import__("0-read_file").read_file
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 path = sys.argv[1]
 
@@ -18,7 +18,8 @@ print("Initial student:")
 print(student_1)
 print(type(student_1))
 print(type(j_student_1))
-print("{} {} {}".format(student_1.first_name, student_1.last_name, student_1.age))
+print("{} {} {}".format(
+    student_1.first_name, student_1.last_name, student_1.age))
 
 
 save_to_json_file(j_student_1, path)
@@ -30,7 +31,11 @@ print("Fake student:")
 new_student_1 = Student("Fake", "Fake", 89)
 print(new_student_1)
 print(type(new_student_1))
-print("{} {} {}".format(new_student_1.first_name, new_student_1.last_name, new_student_1.age))
+print(
+    "{} {} {}".format(
+        new_student_1.first_name, new_student_1.last_name, new_student_1.age
+    )
+)
 
 
 print("Load dictionary from file:")
@@ -39,4 +44,8 @@ new_j_student_1 = load_from_json_file(path)
 new_student_1.reload_from_json(j_student_1)
 print(new_student_1)
 print(type(new_student_1))
-print("{} {} {}".format(new_student_1.first_name, new_student_1.last_name, new_student_1.age))
+print(
+    "{} {} {}".format(
+        new_student_1.first_name, new_student_1.last_name, new_student_1.age
+    )
+)
