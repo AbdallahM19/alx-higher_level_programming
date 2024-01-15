d85205eb15d27c6da7d73e80a7b329219ce637b1
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-01-15 04:31:44 +0200

    circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """Base class"""
 
 import json
+import csv
 
 
 class Base:
@@ -68,3 +69,38 @@ class Base:
                 return [cls.create(**i) for i in cls.from_json_string(content)]
         except FileNotFoundError:
             return []
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """Serialize instances to CSV file"""
+        filename = cls.__name__ + ".csv"
+        with open(filename, "w", newline='') as csvfile:
+            if list_objs is not None or list_objs != []:
+                header = cls.get_csv_header()
+                csv_line = csv.DictWriter(csvfile, fieldnames=header)
+                for obj in list_objs:
+                    csv_line.writerow(obj.to_dictionary())
+
+    
+    @classmethod
+    def load_from_file_csv(cls):
+        """Deserialize instances from CSV file"""
+        filename = cls.__name__ + ".csv"
+        try:
+            with open(filename, "r", newline="") as csvfile:
+                header = cls.get_csv_header()
+                lines = csv.DictReader(csvfile, fieldnames=header)
+                lines = [dict([k, int(v)] for k, v in d.items())
+                            for d in lines]
+                return [cls.create(**i) for i in lines]
+        except (FileNotFoundError, IOError):
+            return []
+
+
+    @classmethod
+    def get_csv_header(cls):
+        """Get CSV header for the class"""
+        if cls.__name__ == "Rectangle":
+            return ["id", "width", "height", "x", "y"]
+        else:
+            return ["id", "size", "x", "y"]
