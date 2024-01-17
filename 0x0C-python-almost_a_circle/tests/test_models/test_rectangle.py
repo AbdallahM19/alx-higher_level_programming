4210f113f3c95f6a3f8010176b9bacb8436147f5
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-01-17 04:54:42 +0200

    circle

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -36,6 +36,34 @@ class TestRectangle(unittest.TestCase):
         with self.assertRaises(ValueError):
             rect.width = -5
 
+    def test_rectangle_1_2_exists(self):
+        rectangle = Rectangle(1, 2)
+        self.assertIsInstance(rectangle, Rectangle)
+
+    def test_rectangle_1_2_3_exists(self):
+        rectangle = Rectangle(1, 2, 3)
+        self.assertIsInstance(rectangle, Rectangle)
+
+    def test_rectangle_1_2_3_4_exists(self):
+        rectangle = Rectangle(1, 2, 3, 4)
+        self.assertIsInstance(rectangle, Rectangle)
+
+    def test_invalid_width_type(self):
+        with self.assertRaises(TypeError):
+            rectangle = Rectangle("1", 2)
+
+    def test_invalid_height_type(self):
+        with self.assertRaises(TypeError):
+            rectangle = Rectangle(1, "2")
+
+    def test_invalid_x_type(self):
+        with self.assertRaises(TypeError):
+            rectangle = Rectangle(1, 2, "3")
+
+    def test_invalid_y_type(self):
+        with self.assertRaises(TypeError):
+            rectangle = Rectangle(1, 2, 3, "4")
+
     def test_height_setter(self):
         rect = Rectangle(5, 10)
         rect.height = 20
@@ -125,50 +153,6 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(
             rect_dict, {"id": 7, "width": 8, "height": 15, "x": 12, "y": 8}
         )
-
-
-"""-----------------------------------"""
-
-
-class TestRectangle(unittest.TestCase):
-    def inheritance(self):
-        self.assertIsInstance(Rectangle(6, 3), Base)
-        r2 = Rectangle(6, 7)
-        self.assertIsInstance(r2, Base)
-        self.assertIsInstance(r2, Rectangle)
-
-    def test_args(self):
-        with self.assertRaises(TypeError):
-            Rectangle(1)
-
-        with self.assertRaises(TypeError):
-            Rectangle()
-
-        with self.assertRaises(TypeError):
-            Rectangle(8, 7, 9, 3, 8, 5, 2)
-
-    def test_ides(self):
-        r1 = Rectangle(4, 6, 8, 4, 50)
-        r2 = Rectangle(1, 3)
-        r3 = Rectangle(5, 8, 7)
-        r4 = Rectangle(4, 8, 5, 8, 45)
-        r5 = Rectangle(8, 7, 5, 4)
-        self.assertEqual(r1.id, 50)
-        self.assertEqual(r2.id, 3)
-        self.assertEqual(r3.id, 4)
-        self.assertEqual(r4.id, 45)
-        self.assertEqual(r5.id, 5)
-
-    def test_more_args(self):
-        rec = Rectangle(4, 2, 6)
-        self.assertEqual(rec.y, 0)
-
-        rec = Rectangle(7, 8, 9, 4)
-        rec1 = Rectangle(6, 8)
-        self.assertEqual(rec.id, rec1.id - 1)
-
-        rec = Rectangle(8, 4)
-        self.assertEqual(rec.x, 0)
 
 
 if __name__ == "__main__":
