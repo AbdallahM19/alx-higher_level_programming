9d93bf03508c0e1648d0cd9f2b99572ac0e695ab
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-02-15 04:15:30 +0200

    more-sql

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,10 +1,10 @@
--- lists all shows without the genre Comedy in the database hbtn_0d_tvshows.
-SELECT tv_shows.title
-FROM tv_shows WHERE id NOT IN (
-    SELECT tv_shows.id
-    FROM tv_shows
-    JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-    JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-    WHERE tv_genres.name = 'Comedy'
+-- uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter
+SELECT name
+FROM tv_genres WHERE name NOT IN (
+    SELECT tv_genres.name
+    FROM tv_genres
+    JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+    JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+    WHERE tv_shows.title = 'Dexter'
 )
-ORDER BY tv_shows.title;
+ORDER BY name;
