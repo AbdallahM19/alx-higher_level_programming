764798a77c8f0ff36281033ae1b9ee5a41067ff8
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-03-12 01:32:11 +0200

    java

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const argc = parseInt(process.argv[2]);
+if (!isNaN(argc)) {
+  console.log('My number: ' + argc);
+} else {
+  console.log('Not a number');
+}
---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+
+const argc = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let index = 0; index < argc.length; index++) {
+  console.log(argc[index]);
+}
