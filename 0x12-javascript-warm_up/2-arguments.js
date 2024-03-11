f0fd0cc874556d33fb02213268d67f1018155b97
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-03-12 00:58:08 +0200

    java

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
-const argc = process.argv.lenght;
 
-if (argc === 0) {
+const argc = process.argv.length;
+
+if (argc === 2) {
   console.log('No argument');
-} else if (argc === 1) {
+} else if (argc === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
