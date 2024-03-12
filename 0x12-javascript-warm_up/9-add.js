2d0e95f4e0e01f43a9a4f936467073b23773005e
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-03-12 02:04:36 +0200

    java

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+function add (a, b) {
+  return (a + b);
+}
+const argcone = parseInt(process.argv[2]);
+const argctwo = parseInt(process.argv[2]);
+
+if (!isNaN(argcone) && !isNaN(argctwo)) {
+  console.log(add(argcone, argctwo));
+} else {
+  console.log('NaN');
+}
