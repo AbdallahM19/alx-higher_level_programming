313468ca6c8472b5ee5af408e83c6b9f70540ce7
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-03-13 01:25:46 +0200

    javascript

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0 && !isNaN(w) && !isNaN(h)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const Myvar = this.height;
+    this.height = this.width;
+    this.width = Myvar;
+  }
+
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
+}
+
+module.exports = Rectangle;
