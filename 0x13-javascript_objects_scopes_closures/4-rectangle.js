4b27160378629ed75ea5e88cb34c28135d1903ef
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-03-13 01:32:55 +0200

    javascript

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -2,28 +2,36 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0 && !isNaN(w) && !isNaN(h)) {
+    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
+      this.width = undefined;
+      this.height = undefined;
+    } else {
       this.width = w;
       this.height = h;
     }
   }
 
   print () {
-    for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
+    if (this.width !== undefined && this.height !== undefined) {
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+      }
     }
   }
 
   rotate () {
-    let Myvar = 0;
-    Myvar = this.height;
-    this.height = this.width;
-    this.width = Myvar;
+    if (this.width !== undefined && this.height !== undefined) {
+      const temp = this.height;
+      this.height = this.width;
+      this.width = temp;
+    }
   }
 
   double () {
-    this.height *= 2;
-    this.width *= 2;
+    if (this.width !== undefined && this.height !== undefined) {
+      this.width *= 2;
+      this.height *= 2;
+    }
   }
 }
 
