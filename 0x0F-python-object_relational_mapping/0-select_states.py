2f28084ed7973c78b95cdb231b114be9b264d594
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-03-14 11:56:46 -0700

    alx-sand

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Write a script that lists all states
+from the database 'hbtn_0e_0_usa':
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    """
+    Your script should take 3 arguments:
+    mysql username
+    mysql password
+    database name (no argument validation needed)
+    """
+    args = MySQLdb.connect(
+        host = "localhost",
+        port = 3306,
+        user = sys.argv[1],
+        passwd = sys.argv[2],
+        name = sys.argv[3]
+    )
+    cur = args.cursor()
+    cur.execute("SELECT * FROM states")
+    row = cur.fetchall()
+    for i in row:
+        print(i)
+    cur.close()
+    args.close()
