39ee3f70630d8c3005d095fa2aab74352ac17ee9
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-03-15 16:34:55 +0200

    mapping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,7 +7,7 @@ from the database 'hbtn_0e_0_usa':
 import MySQLdb
 import sys
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     """
     Your script should take 3 arguments:
     mysql username
@@ -15,11 +15,11 @@ if __name__ == '__main__':
     database name (no argument validation needed)
     """
     args = MySQLdb.connect(
-        host = "localhost",
-        port = 3306,
-        user = sys.argv[1],
-        passwd = sys.argv[2],
-        name = sys.argv[3]
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        name=sys.argv[3],
     )
     cur = args.cursor()
     cur.execute("SELECT * FROM states")
