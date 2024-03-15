2237e826e24c5e723e68ab786ccb60b612b8f75c
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-03-15 19:40:39 +0200

    mapping

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -21,18 +21,19 @@ if __name__ == "__main__":
         port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.argv[3]
+        db=sys.argv[3],
     )
     cur = args_cur.cursor()
     cur.execute(
         "SELECT cities.id, cities.name FROM cities \
         JOIN states ON cities.state_id = states.id \
         WHERE states.name = %(name)s \
-        ORDER BY cities.id ASC", {'name': sys.argv[4]}
+        ORDER BY cities.id ASC",
+        {"name": sys.argv[4]},
     )
     rows = cur.fetchall()
     res = [row[1] for row in rows]
-    print(', '.join(res))
+    print(", ".join(res))
 
     cur.close()
     args_cur.close()
