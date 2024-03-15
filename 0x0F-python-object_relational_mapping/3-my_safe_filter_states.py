b7c4ba3a7321df6f349258a9a59710575810c85f
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-03-15 17:42:59 +0200

    mapping

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -22,13 +22,13 @@ if __name__ == "__main__":
         port=3306,
         user=sys.argv[1],
         password=sys.argv[2],
-        data=sys.argv[3]
+        data=sys.argv[3],
     )
     cur = args_cur.cursor()
     cur.execute(
         "SELECT * FROM states WHERE name LIKE \
-        BINARY %(name)s ORDER BY states.id ASC", \
-        {'name': sys.argv[4]}
+        BINARY %(name)s ORDER BY states.id ASC",
+        {"name": sys.argv[4]},
     )
     row = cur.fetchall()
     for i in row:
