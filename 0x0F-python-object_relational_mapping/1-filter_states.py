1c4d7d64fd7d822bfcd6c4f0821f4f327ffc5bb6
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-03-15 17:24:28 +0200

    mapping

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
-Write a script that takes in an argument
-displays all values in the states
-table of hbtn_0e_0_usa where name matches the argument.
+Write a script that lists all states with
+a name starting with N (upper N)
+from the database hbtn_0e_0_usa:
 """
 
 import MySQLdb
@@ -10,11 +10,10 @@ import sys
 
 if __name__ == "__main__":
     """
-    Your script should take 4 arguments:
+    Your script should take 3 arguments:
     mysql username
     mysql password
-    database name and state name searched
-    (no argument validation needed)
+    database name (no argument validation needed)
     """
     args_cur = MySQLdb.connect(
         host="localhost",
@@ -25,8 +24,8 @@ if __name__ == "__main__":
     )
     cur = args_cur.cursor()
     cur.execute(
-        "SELECT * FROM states WHERE name LIKE BINARY '{}' \
-        ORDER BY states.id ASC",(sys.argv[4],),
+        "SELECT * FROM states WHERE name LIKE BINARY 'N%' \
+        ORDER BY states.id ASC"
     )
     row = cur.fetchall()
     for i in row:
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -21,13 +21,12 @@ if __name__ == "__main__":
         port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.argv[3],
+        db=sys.argv[3]
     )
     cur = args_cur.cursor()
     cur.execute(
-        "SELECT * FROM states WHERE name = %s \
-        ORDER BY states.id ASC",
-        (sys.argv[4],),
+        "SELECT * FROM states WHERE name LIKE BINARY '{}' \
+        ORDER BY states.id ASC", (sys.argv[4],),
     )
     row = cur.fetchall()
     for i in row:
