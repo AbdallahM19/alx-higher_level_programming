b42ab5ef299c142c78b084878ab0d5e899a429dc
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-03-16 04:44:03 +0200

    mapping

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Write a script that adds the State object
+“Louisiana” to the database hbtn_0e_6_usa
+"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def model_state_fetch_first(user, password, name):
+    """
+    Your script should take 3 arguments:
+    mysql username
+    mysql password
+    database name
+    """
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            user, password, name
+        ), pool_pre_ping=True
+    )
+    Session = sessionmaker(bind=engine)
+    Sess = Session()
+    state = Sess.query(State).filter(State.id == 2).first()
+    state.name = 'New Mexico'
+    Sess.commit()
+    engine.close()
+
+
+if __name__ == "__main__":
+    if len(argv) == 4:
+        username, password, db_name = argv[1], argv[2], argv[3]
+        # search = argv[4]
+        model_state_fetch_first(username, password, db_name)
