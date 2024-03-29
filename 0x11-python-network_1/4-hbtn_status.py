913cd64e72739a064a54116daaf03451178e3f93
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-03-28 18:22:06 -0700

    net_2

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
 """
-Write a Python script that takes in a URL,
-sends a request to the URL and displays the body of
-the response (decoded in utf-8).
+Write a Python script that fetches
+https://alx-intranet.hbtn.io/status
 """
-from sys import argv
-from urllib.error import HTTPError
-from urllib.request import urlopen
-from urllib.request import Request
+import requests
 
 
 if __name__ == "__main__":
-    req = Request(argv[1])
-    try:
-        with urlopen(req) as res:
-            print(res.read().decode('utf-8'))
-    except HTTPError as e:
-        print("Error code: {}".format(e.code))
+    res = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))
