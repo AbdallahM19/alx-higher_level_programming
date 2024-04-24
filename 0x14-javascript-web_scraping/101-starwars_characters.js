3f1ea4a1253b36f0e5fb3792cb14cbe6330c7291
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-04-23 18:32:41 -0700

    readme_js

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const request = require('request');
+const id = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  const movie = JSON.parse(body);
+  movie.characters.forEach(chars => {
+    request.get(chars, (error, response, body) => {
+      if (error) {
+        console.log(error);
+        return;
+      }
+      const i = JSON.parse(body);
+      console.log(i.name);
+    });
+  });
+});
